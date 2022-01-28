034fc67dbf7aded868f0003bb53095b843889b32
olamide2011 <91997622+olamide2011@users.noreply.github.com>
2022-01-27 20:02:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+
+});
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LGRN1b6qV/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model is loaded")
+}
+
+function check() {
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=floor(results[0].confidence*100);
+        
+    
+    }
+    }        
